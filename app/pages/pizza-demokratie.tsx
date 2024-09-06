2efc5d16442884455644d01cefe47fc9f873fc35
sansan88 <sandro.scalco@liitu.ch>
2024-09-06 09:27:32 +0200

    fix layout

--- app/pages/pizza-demokratie.tsx
+++ app/pages/pizza-demokratie.tsx
@@ -12,8 +12,6 @@ import { Alert, AlertDescription } from '@/components/ui/alert';
 
 type CantonKeys = 'ZH' | 'BE' | 'LU' | 'UR' | 'SZ' | 'OW' | 'NW' | 'GL' | 'ZG' | 'FR' | 'SO' | 'BS' | 'BL' | 'SH' | 'AR' | 'AI' | 'SG' | 'GR' | 'AG' | 'TG' | 'TI' | 'VD';
 
-
-
 const cantonData: Record<CantonKeys, {
     name: string;
     population: number;
